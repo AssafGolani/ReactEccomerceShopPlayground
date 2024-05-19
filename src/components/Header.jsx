f663f73f17dafac60ab8ee2254f027986ed95c0a
Assaf <1214assaf@gmail.com>
2024-05-19 09:15:51 +0300

    feat: Add Cart component for displaying the shopping cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import CartContext from "../store/CartContext";
 import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
   const totalCartItems = cartCtx.items.reduce(
     (acc, item) => acc + item.quantity,
     0
   );
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
   return (
     <header id="main-header">
       <div id="title">
@@ -17,7 +23,9 @@ export default function Header() {
       </div>
 
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button onClick={handleShowCart} textOnly>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,12 +1,10 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 export function Modal({ children, open, className = "" }) {
   const dialog = useRef();
   useEffect(() => {
     if (open) {
       dialog.current.showModal();
-    } else {
-      dialog.current.close();
     }
   }, [open]);
   return createPortal(
