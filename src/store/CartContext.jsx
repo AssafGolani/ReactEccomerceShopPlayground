af87b6a0db94fd65a841db94ee962a71d26fc414
Assaf <1214assaf@gmail.com>
2024-05-19 10:09:31 +0300

    Refactor CartContext to use existing item in REMOVE_ITEM case

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -7,6 +7,7 @@ const CartContext = createContext({
   removeItem: (id) => {},
   clearCart: () => {},
 });
+
 function cartReducer(state, action) {
   switch (action.type) {
     case "ADD_ITEM":
@@ -14,6 +15,7 @@ function cartReducer(state, action) {
         (item) => item.id === action.item.id
       );
       const updatedItems = [...state.items];
+
       if (existingCartItemIndex > -1) {
         const existingItem = state.items[existingCartItemIndex];
         const updatedItem = {
@@ -27,27 +29,37 @@ function cartReducer(state, action) {
           quantity: 1,
         });
       }
+
       return {
         ...state,
         items: updatedItems,
         totalAmount: state.totalAmount + action.item.price,
       };
+
     case "REMOVE_ITEM":
-      const existingItem = state.items[existingCartItemIndex];
-      const updatedTotalAmount = state.totalAmount - existingItem.price;
+      const existingItemIndex = state.items.findIndex(
+        (item) => item.id === action.id
+      );
+      const existingItem = state.items[existingItemIndex];
+      let updatedTotalAmount = state.totalAmount - existingItem.price;
+      let updatedItemsForRemove;
+
       if (existingItem.quantity === 1) {
-        updatedItems = state.items.filter((item) => item.id !== action.id);
+        updatedItemsForRemove = state.items.filter(
+          (item) => item.id !== action.id
+        );
       } else {
         const updatedItem = {
           ...existingItem,
           quantity: existingItem.quantity - 1,
         };
-        updatedItems = [...state.items];
-        updatedItems[existingCartItemIndex] = updatedItem;
+        updatedItemsForRemove = [...state.items];
+        updatedItemsForRemove[existingItemIndex] = updatedItem;
       }
+
       return {
         ...state,
-        items: updatedItems,
+        items: updatedItemsForRemove,
         totalAmount: updatedTotalAmount,
       };
 
@@ -56,15 +68,18 @@ function cartReducer(state, action) {
         items: [],
         totalAmount: 0,
       };
+
     default:
       return state;
   }
 }
+
 export function CartContextProvider({ children }) {
   const [cart, dispatchCartAction] = useReducer(cartReducer, {
     items: [],
     totalAmount: 0,
   });
+
   const cartContext = {
     items: cart.items,
     totalAmount: cart.totalAmount,
@@ -78,7 +93,7 @@ export function CartContextProvider({ children }) {
       dispatchCartAction({ type: "CLEAR_CART" });
     },
   };
-  console.log(cartContext);
+
   return (
     <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
   );
