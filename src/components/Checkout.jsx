70476e197c7ba655cdb948920b794d33a6f72a13
Assaf <1214assaf@gmail.com>
2024-05-19 10:46:11 +0300

    feat: Implement form submission in Checkout component
    The Checkout component now includes a form submission handler that sends a POST request to the server with the order details, including the items in the cart and customer information.

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -16,15 +16,35 @@ export default function Checkout() {
   function handleClose() {
     userProgressCtx.hideCheckout();
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const customerData = Object.fromEntries(formData.entries());
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
+  }
   return (
     <Modal
       open={userProgressCtx.userProgress === "checkout"}
       onClose={handleClose}
     >
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="Email Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
