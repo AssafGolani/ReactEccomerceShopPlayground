f6c2c966a525a315303301eeeba188f572391117
Assaf <1214assaf@gmail.com>
2024-05-19 11:45:53 +0300

    feat: Add Error component for displaying error messages
    The Error component is added to handle displaying error messages in the UI. It accepts a title and message as props and renders them in a styled div. This improves the user experience by providing clear feedback when errors occur.

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import { MealItem } from "./MealItem";
 import useHttp from "../hooks/useHttp";
+import Error from "./Error";
 const requestConfig = {};
 export default function Meals() {
   const {
@@ -8,7 +9,10 @@ export default function Meals() {
     error,
   } = useHttp("http://localhost:3000/meals", requestConfig, []);
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <p className="center">Loading...</p>;
+  }
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error} />;
   }
   return (
     <ul id="meals">
